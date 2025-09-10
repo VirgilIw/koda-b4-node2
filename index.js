96bcd235673fc46b62848eb4971878225392518a
VirgilIw <imanuelambar11@gmail.com>
2025-09-10 08:58:02 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// import { manualDate } from "./lib/manual.js";
+// import { manualPakaiAsync } from "./lib/manualTanpaMoment.js";
+import { pakaiMoment } from "./lib/momentDate.js";
+// import { tanpaMomentThen } from "./lib/tanpaMoment.js";
+
+// manualPakaiAsync();
+pakaiMoment();
+// tanpaMomentThen();
+// manualDate();
